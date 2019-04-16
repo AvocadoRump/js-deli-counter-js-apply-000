3d52022f3c1ee457911c4a6746aea6d4522f4f93
punctual-skill-3845 <bichsonnguyen@gmail.com>
2019-04-16 00:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currentLine, newPerson) {
   currentLine.push(newPerson)
-  return `Welcome, ${newPerson}. You are number ${currentLine.length + 1} in line.`
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
 }
 
 function nowServing(currentLine) {
