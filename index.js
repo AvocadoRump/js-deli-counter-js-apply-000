63540c7f10a53df7aba67879bfb72dd65f7be841
punctual-skill-3845 <bichsonnguyen@gmail.com>
2019-04-16 00:35:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 function takeANumber(currentLine, newPerson) {
-  let humanWord;
-  switch (currentLine.length) {
-    case 1: humanWord = '1st'; break;
-    case 2: humanWord = '2nd'; break;
-    case 3: humanWord = '3rd'; break;
-    default: humanWord = `${currentLine}th`;
-  }
-  return `Welcome, ${newPerson}. You are ${humanWord} in line.`
+  return `Welcome, ${newPerson}. You are number ${currentLine} in line.`
 }
 
 function nowServing(currentLine) {
